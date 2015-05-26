6ee3e6082380d2f188fd6e43fc8bd49b9e2dc3db
diego <diego@healthcare.com>
2015-05-26 16:04:39 -0600

    Removed the ipware package and added support for CORS

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ var get_ip = require('./node_modules/ipware')().get_ip;
 app = express();
 
   
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
 
